6eb9c693afff56e5430b878728c87b78e81b3de8
Alyssa Moody and Buna Ezeofor <alyssa+buna@>
2017-05-10 14:33:32 -0700

    Create objects. Re-do click function to call objects. WORK IN PROGRESS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,73 @@
 //Business Logic
 
-// function Player(name) {
-//   this.name = name;
-//   this.turnScore = 0;
-//   this.totalScore = 0;
-//   this.isTurn = false;
-//   this.dice = new Dice;
-// }
-//
-// Player.prototype.newGame = function () {
-//   this.roll = 0;
-//   this.tempscore = 0;
-//   this.totalscore = 0;
-// }
-
-
-function randomNumber() {
-  return Math.floor((Math.random() * 6) + 1);
-};
-
-var sumElements = function(someArray) {
-    if (someArray == null) return false;
-    var sum = 0;
-    for (var i = 0, len = someArray.length; i < len; i++) {
-        sum += someArray[i];
-    }
-    return sum;
+function Player(score, turnScore) {
+  this.score = score;
+  this.turnScore = turnScore;
 }
 
-// function player1TotalScore() {
-//   var = '#player1roll' +
+Player.prototype.rollDie = function(){
+  // var rollScore = 0;
+  var diceArray = [];
+
+  var rollScore = Math.floor((Math.random() * 6) + 1);
+  if (rollScore !== 1){
+    this.turnScore += rollScore;
+  }else{
+    rollScore = 0;
+    this.turnScore = 0;
+    return "You rolled a one!";
+  }
+
+  diceArray.push(rollScore);
+  return diceArray;
+};
+
+
+
+// Player.prototype.hold = function() {
+//   this.score += this.turnScore
+//   this.turnScore = 0;
 // }
+//
+// Player.prototype.newTurn = function() {
+//   this.turnScore = 0;
+// };
+//
+// Player.prototype.scoreCheck = function() {
+//   if(this.score >= 100){
+//     return "Win";
+//   };
+// };
+//
+// Player.prototype.newGame = function(){
+//   this.turnScore = 0;
+//   this.score = 0;
+// }
+
 
 //User Interface
 $(document).ready(function() {
-
-  // debugger;
   $("#names").submit(function(event) {
     event.preventDefault();
-    var player1 = $('input#player1name').val();
-    var player2 = $('input#player2name').val();
-    $(".player1input").text(player1);
-    $(".player2input").text(player2);
+    var player1Input = $('input#player1name').val();
+    var player2Input = $('input#player2name').val();
+    $(".player1Input").text(player1Input);
+    $(".player2Input").text(player2Input);
 
     $("#names").hide();
     $("#game").show();
-  });
-
-  $("#play-one").click(function() {
-    var numberRolled = randomNumber();
-    $("#player1roll").text(numberRolled);
-
-    var turnScoreArray = [];
-    turnScoreArray.push(numberRolled);
-    sumElements(turnScoreArray);
-
-    $("#player1score").text(turnScoreArray);
 
   });
 
+    $("#play-one").click(function() {
+      var player1 = new Player(0, 0);
+      var player2 = new Player(0, 0);
+
+      var player1Dice = player1.rollDie();
+      $("#player1Roll").text(player1Dice);
+      $("#player1Score").text(player1.turnScore);
+      $("#player-1-total-score").text(player1.score);
+
+
+  });
 });
